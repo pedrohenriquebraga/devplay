e8a5b2238ce1c227be2b271949bf77a944b17008
pedrohenriquebraga <gamesantos7@gmail.com>
2020-12-14 22:46:11 -0300

    Start Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/global.css"
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <title>DevPlay</title>
+    </Head>
+    <Component {...pageProps} />
+  </>  
+  )
 }
 
 export default MyApp
