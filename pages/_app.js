61de8988b5764b69ff558d168644c885442cc851
pedrohenriquebraga <gamesantos7@gmail.com>
2020-12-20 16:10:42 -0300

    Fix tags

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 function MyApp({ Component, pageProps }) {
   return (
   <>
-    <Html lang="pt-br">
       <Head>
          <title>DevPlay</title>
          <meta name="description" content="Veja, publique e compartilhe vídeos de desenvolvedores de todo o mundo!" />
       </Head>
-    </Html>
     <Component {...pageProps} />
   </>  
   )
