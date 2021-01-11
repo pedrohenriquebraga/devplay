7fab9abfad98dbfb742958a533828fe58318953a
pedrohenriquebraga <gamesantos7@gmail.com>
2021-01-11 19:30:39 -0300

    Add actions on player

--- src/components/VideoInfos.js
+++ src/components/VideoInfos.js
@@ -1,17 +1,54 @@
 import Link from "next/link";
+import { BiLike, BiDislike, BiShare } from "react-icons/bi";
+import { AiOutlineWarning } from "react-icons/ai";
+import { MdDateRange, MdPeopleOutline } from "react-icons/md";
 import {
   VideoTitle,
+  VideoStatistics,
   VideoDescription,
   Description,
   ExtraInfos,
 } from "../../styles/VideoInfos";
 
 function VideoInfos() {
+
   return (
     <>
       <VideoTitle>
         Como migrar um projeto de HTML/CSS para ReactJs de forma fácil?
       </VideoTitle>
+      <VideoStatistics>
+        <p className="postedDate">
+          <MdDateRange /> 28/01/2020
+        </p>
+        <p className="views">
+          <MdPeopleOutline /> 280.958 visualizações
+        </p>
+        <div className="ActionsContainer">
+          <div className="RateContainer">
+            <div className="Like Rate">
+              <button className="LikeButton RateButton">
+                <BiLike />
+                <span className="RateCounter LikeCounter">100 mil</span>
+              </button>
+            </div>
+            <div className="Dislike Rate">
+              <button className="DisLikeButton RateButton">
+                <BiDislike />
+                <span className="RateCounter DisLikeCounter">101</span>
+              </button>
+            </div>
+          </div>
+          <button className="ShareButton">
+            <BiShare />
+            <span>Compartilhar</span>
+          </button>
+          <button className="DenounceButton">
+            <AiOutlineWarning />
+            <span>Denunciar</span>
+          </button>
+        </div>
+      </VideoStatistics>
       <VideoDescription>
         <summary>Mais informações</summary>
         <Description>
@@ -25,25 +62,25 @@ function VideoInfos() {
           🔔 Ative às notificações
         </Description>
         <ExtraInfos>
-            <div className="infos">
-              <p className="categories">
-                <strong>Categorias: </strong>
-                <Link href="#">
-                  <a>JavaScript</a>
-                </Link>
-                ,{" "}
-                <Link href="#">
-                  <a>Web design</a>
-                </Link>
-              </p>
-              <p className="license">
-                <strong>Licença: </strong>
-                <Link href="#">
-                  <a>Padrão Devplay</a>
-                </Link>
-              </p>
-            </div>
-          </ExtraInfos>
+          <div className="infos">
+            <p className="categories">
+              <strong>Categorias: </strong>
+              <Link href="#">
+                <a>JavaScript</a>
+              </Link>
+              ,{" "}
+              <Link href="#">
+                <a>Web design</a>
+              </Link>
+            </p>
+            <p className="license">
+              <strong>Licença: </strong>
+              <Link href="#">
+                <a>DevPlay</a>
+              </Link>
+            </p>
+          </div>
+        </ExtraInfos>
       </VideoDescription>
     </>
   );
--- styles/VideoInfos.js
+++ styles/VideoInfos.js
@@ -1,17 +1,88 @@
-import styled from "styled-components";
-
+import styled, { keyframes } from "styled-components";
 
 export const VideoTitle = styled.h1`
-  width: 80%;
-  padding: 0.5rem 0.3rem;
+  width: 100%;
+  padding: 0.5rem 1rem;
   font-family: "Nunito Sans", sans-serif;
   color: #111;
 `;
 
+export const VideoStatistics = styled.div`
+  width: 100%;
+  margin-bottom: 1rem;
+  p {
+    margin-left: 1.0rem;
+    font-size: 1.6rem;
+    display: inline;
+    margin-right: 1rem;
+
+    svg {
+      font-size: x-large;
+      vertical-align: sub;
+    }
+  }
+
+  .ActionsContainer {
+    overflow-x: auto;
+    padding: 0 1rem;
+    margin: 1rem 0;
+    button {
+      cursor: pointer;
+      width: 11rem;
+      margin-right: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 1rem;
+      font-size: 1.6rem;
+      font-weight: 600;
+      border: none;
+      outline: none;
+      background: transparent;
+      transition: 300ms;
+      svg {
+        font-size: x-large;
+      }
+
+      :hover {
+        background-color: #eee;
+        border-radius: 5px;
+      }
+    }
+
+    .DenounceButton {
+      svg {
+        fill: #ff3341;
+      }
+    }
+
+    .ShareButton {
+      svg {
+        fill: #6f86d6;
+      }
+    }
+
+    display: flex;
+    width: 100%;
+    .RateContainer {
+      display: flex;
+      .Rate {
+        .RateButton {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;  
+        }
+      }
+    }
+  }
+`;
+
 export const VideoDescription = styled.details`
   width: 100%;
-  padding: 2rem 0.3rem;
-  background: linear-gradient(to right, #eee, #ddd);
+  padding: 2rem 1rem;
+  background: #eee;
+  transition: 500ms;
 
   summary {
     font-size: 1.8rem;
@@ -24,7 +95,6 @@ export const VideoDescription = styled.details`
 export const Description = styled.p`
   font-size: 1.8rem;
   padding: 1rem 0.3rem;
-
 `;
 
 export const ExtraInfos = styled.div`
@@ -33,5 +103,10 @@ export const ExtraInfos = styled.div`
     display: flex;
     flex-direction: column;
     font-size: 1.6rem;
+
+    a {
+      text-decoration: none;
+      font-weight: 600;
+    }
   }
 `;
